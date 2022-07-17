ff9f361cecac6de345068133bf1d07735b83bc22
Jon Lund <jon.lund@gmail.com>
2022-07-17 00:40:43 -0600

    added basic keys

--- Sources/Rapyd/Rapyd.swift
+++ Sources/Rapyd/Rapyd.swift
@@ -1,6 +1,18 @@
-public struct Rapyd {
-    public private(set) var text = "Hello, World!"
+private var _rapyd: Rapyd?
 
-    public init() {
+public struct Rapyd {
+	private let secretKey: String
+	private let accessKey: String
+
+	
+	public static var shared: Rapyd? { _rapyd }
+	
+	public static func setShared(_ singleton: Rapyd?) {
+		_rapyd = singleton
+	}
+
+	public init(secretKey: String, accessKey: String) {
+		self.secretKey = secretKey
+		self.accessKey = accessKey
     }
 }
